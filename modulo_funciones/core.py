b890ba6fa5f4515c0612a42b8a387e2af9ee5f44
christopherhess0 <83043287+christopherhess0@users.noreply.github.com>
2024-10-31 17:23:08 -0300

    Agregue cartas.txt para importar como archivo json las cartas

--- modulo_funciones/core.py
+++ modulo_funciones/core.py
@@ -4,25 +4,20 @@ import time
 import hashlib
 import re
 import getpass
+import json
 
 '''
 Mazo
 '''
+ruta_carpeta = os.path.dirname(__file__)
+ruta_cartas = os.path.join(ruta_carpeta, "cartas.txt")
 
-cartas = [
-    ('1', 'Espada', 14), ('1', 'Basto', 13), 
-    ('7', 'Espada', 12), ('7', 'Oro', 11),
-    ('3', 'Espada', 10), ('3', 'Basto', 10), ('3', 'Copa', 10), ('3', 'Oro', 10),
-    ('2', 'Espada', 9), ('2', 'Basto', 9), ('2', 'Copa', 9), ('2', 'Oro', 9),
-    ('1', 'Copa', 8), ('1', 'Oro', 8),
-    ('12', 'Espada', 7), ('12', 'Basto', 7), ('12', 'Oro', 7), ('12', 'Copa', 7),
-    ('11', 'Espada', 6), ('11', 'Basto', 6), ('11', 'Oro', 6), ('11', 'Copa', 6),
-    ('10', 'Espada', 5), ('10', 'Basto', 5), ('10', 'Oro', 5), ('10', 'Copa', 5),
-    ('7', 'Copa', 4), ('7', 'Basto', 4),
-    ('6', 'Espada', 3), ('6', 'Basto', 3), ('6', 'Oro', 3), ('6', 'Copa', 3),
-    ('5', 'Espada', 2), ('5', 'Basto', 2), ('5', 'Oro', 2), ('5', 'Copa', 2),
-    ('4', 'Espada', 1), ('4', 'Basto', 1), ('4', 'Oro', 1), ('4', 'Copa', 1),
-    ]
+# Abre el archivo usando la ruta completa
+with open(ruta_cartas, "r") as archivo:
+    cartas = json.load(archivo)
+    cartas = [tuple(carta) for carta in cartas]
+
+
 
 '''
 Mezclar el mazo de forma aleatoria
